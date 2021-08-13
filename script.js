c74c6911fa8fedab5c9e256271e88d67b65b2048
hinanshi4 <hinanshis4@gmail.com>
2021-08-13 15:58:55 +0530

    fix: updated values in table

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const billAmount = document.querySelector("#bill-amount");
 const nextButton = document.querySelector("#next-button");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 nextButton.addEventListener("click", nextClickEventHandler);
 
 function nextClickEventHandler(){
-    if(billAmount.value>0){
+    if(parseInt(billAmount.value)>0){
         document.getElementById("second").style.display="block";
         document.getElementById("error1").style.display="none";
     }
@@ -19,7 +19,7 @@ function nextClickEventHandler(){
 checkButton.addEventListener("click", CheckClickEventHandler);
 
 function CheckClickEventHandler(){
-    if(billAmount.value<=cashGiven.value){
+    if(parseInt(billAmount.value)<=parseInt(cashGiven.value)){
         // if(billAmount.value===cashGiven.value){
         //     document.getElementById("error2").innerText="No change is to be given.";
         // }
@@ -27,7 +27,7 @@ function CheckClickEventHandler(){
         document.getElementById("error2").style.display="none";
 
         const amountToBeReturned = cashGiven.value - billAmount.value;
-        calculateChange=(amountToBeReturned);
+        calculateChange(amountToBeReturned);
     }
     document.getElementById("error2").innerText="Enter valid bill amount and cash given to calculate";
 }
